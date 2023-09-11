496d389faefe3977de2d9187890c76862a72ac9f
daniela23ai <degeonu23@gmail.com>
2023-09-11 16:52:30 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
@@ -6,6 +5,6 @@
  */
 int main(void)
 { 
-	fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
 }
