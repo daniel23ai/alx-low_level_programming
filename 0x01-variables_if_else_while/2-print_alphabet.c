1e7c7fd3ab507d1f1a0df66fe5326abd5d54ca20
daniela23ai <degeonu23@gmail.com>
2023-09-13 15:24:13 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Header phrase
+ * Return: 0 (Good)
+ */
+
+int main(void)
+{
+	char c;
+	
+	c = 'b';
+	while 
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
