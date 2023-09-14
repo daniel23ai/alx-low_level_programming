81a99f2af90051f3ce5c947eb5d21d95ba6bfc99
daniela23ai <degeonu23@gmail.com>
2023-09-13 21:38:03 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,10 @@
 
 int main(void)
 {
-	char c;
-	
-	c = 'a';
-	while 
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
