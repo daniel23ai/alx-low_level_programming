84ea1e0dc6d2b8ea2faec38f94ee89e3afd8390f
daniela23ai <degeonu23@gmail.com>
2023-09-22 17:14:05 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,7 +13,7 @@ char *rot13(char *s)
 	char data1[] =
 "ABCDEFGHIJKLMNOPKRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] =
-"NOPQRSTUVWXYZABCDEFGHIJLMnopqrstuvwxyzabcdefghijklm";
+"NOPQRSTUVWXYZABCDEFGHIJkLMnopqrstuvwxyzabcdefghijklm";
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
